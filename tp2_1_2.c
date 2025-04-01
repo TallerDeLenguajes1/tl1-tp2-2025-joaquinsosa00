ebf62d2bb5cca87c96b0eaecd4229b3ddaa278ba
Joaquin <joaquin150496@gmail.com>
2025-04-01 15:06:24 -0300

    Utilizacion de punteros en rama 2

--- tp2_1_2.c
+++ tp2_1_2.c
@@ -6,10 +6,15 @@
 int main() {
 #define N 20
 int i;
-double vt[N];
-for(i = 0;i<N; i++)
-{
-vt[i]=1+rand()%100;
-printf("%.0f \n ", vt[i]);
-}
+    double vt[N];
+
+    
+    double *puntero = vt;  
+
+    for(i = 0; i < N; i++) {
+        *(puntero + i) = 1 + rand() % 100;  
+        printf(" -  %.0f \n",*(puntero + i));  
+    }
+
+    return 0;
 }
